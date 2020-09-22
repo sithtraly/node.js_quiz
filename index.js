0faceff1742fb8c71d4839cc5660e1ae3e50bbdb
sithtraly <70559804+sithtraly@users.noreply.github.com>
2020-09-22 14:38:13 +0700

    quizz complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const lodash = require('lodash');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 
 const app = express()
@@ -23,43 +23,48 @@ app.get(['/', '/users'], (req, res) => {
 app.get(['/:id', '/users/:id'], (req, res) => {
     fs.readFile('userList.json', (err, data) => {
         const json = JSON.parse(data);
-        const users = lodash.find(json, function (item) { return item.id == req.params.id })
-        if (!users) {
-            res.send({ 'message': 'user not found' })
-        } else (
-            res.send(users)
-        )
+        const user = lodash.find(json, (item) => (item.id == req.params.id))
+        !user ? res.send({ 'message': 'user not found' }) : res.send(user)
     })
 })
 
 
 app.post('/add', (req, res) => {
-    var name = req.body.name
-    var password = req.body.password
-    var profession = req.body.profession
-
-    var userAdd
-    fs.readFile('userList.json', (err, data) => {
-        const json = JSON.parse(data)
+    (async () => {
+        const name = req.body.name
+        const password = req.body.password
+        const profession = req.body.profession
+        const users = fs.readFileSync('userList.json')
+        const json = JSON.parse(users)
+        var usersList = json
         const lastUser = lodash.last(json)
-
-        if (name && password && profession) {
-            userAdd = {
-                "id": lastUser.id + 1,
-                "name": name,
-                "password": password,
-                "profession": profession
-            }
+        const existingName = lodash.find(usersList, (item) => (item.name == req.body.name))
+        if (existingName) {
+            res.send({ 'message': 'user already exist' })
+        } else {
+            usersList.push({ "id": lastUser.id + 1, "name": name, "password": password, "profession": profession })
+            fs.writeFile('userList.json', JSON.stringify(usersList), (err) => {
+                err ? res.send({ 'message': 'add user fail' }) : res.send({ 'message': 'add user success' })
+            })
         }
-    })
-            
-    fs.appendFile('userList.json', userAdd, (err) => {
-        if (err) {
-            res.send({ 'message': 'user add fails' })
-        } else (
-            res.send({ 'message': 'user add successfully' })
-        )
-    })    
+    })()
+})
+
+
+app.delete('/delete/:id', (req, res) => {
+    const users = fs.readFileSync('userList.json')
+    var json = JSON.parse(users)
+    const user = lodash.find(json, (item) => (item.id == req.params.id))
+    if (!user) {
+        res.send({ 'message': 'user not found' })
+    } else {
+        json.splice(json.indexOf(user), 1)
+        fs.writeFile('userList.json', JSON.stringify(json), (err) => {
+            err ? res.send({ 'message': 'delete user fail' }) : res.send({ 'message': 'delete user success' })
+        })
+
+    }
+
 })
 
 
